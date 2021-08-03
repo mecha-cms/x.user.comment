964a2fc08d39ebae9b2f112a06e16d2f6c950e67
Taufik Nurrohman <hi@taufik-nurrohman.com>
2021-08-03 18:35:44 +0700

    Update Black-List Data on Hide/Show Comment

--- user.comment/engine/r/route.php
+++ user.comment/engine/r/route.php
@@ -39,6 +39,7 @@ function hit($any) {
     $author = $comment['author'];
     $id = $comment->id;
     $data = \From::page(\file_get_contents($file), true);
+    $has_comment_guard_extension = isset($state->x->{'comment.guard'});
     // Delete comment file permanently
     if ('delete' === $task) {
         if (\unlink($file)) {
@@ -58,6 +59,36 @@ function hit($any) {
     }
     // Change comment status to `-1`
     if ('hide' === $task) {
+        if ($has_comment_guard_extension) {
+            if (isset($data['email']) && \is_file($f = \LOT . \DS . 'x' . \DS . 'comment.guard' . \DS . 'email.txt')) {
+                $exist = false;
+                $test = $data['email'];
+                foreach (\stream($f) as $v) {
+                    if ($test === \trim($v)) {
+                        $exist = true;
+                        break;
+                    }
+                }
+                if (!$exist) {
+                    $n = 0 === \filesize($f) ? "" : "\n";
+                    \file_put_contents($f, $n . $test, \FILE_APPEND | \LOCK_EX);
+                }
+            }
+            if (isset($data['ip']) && \is_file($f = \LOT . \DS . 'x' . \DS . 'comment.guard' . \DS . 'ip.txt')) {
+                $exist = false;
+                $test = $data['ip'];
+                foreach (\stream($f) as $v) {
+                    if ($test === \trim($v)) {
+                        $exist = true;
+                        break;
+                    }
+                }
+                if (!$exist) {
+                    $n = 0 === \filesize($f) ? "" : "\n";
+                    \file_put_contents($f, $n . $test, \FILE_APPEND | \LOCK_EX);
+                }
+            }
+        }
         $data['status'] = -1;
         if (\is_writable($file) && \is_int(\file_put_contents($file, \To::page($data)))) {
             \Alert::success('Comment marked as spam.');
@@ -78,6 +109,34 @@ function hit($any) {
     }
     // Change comment status to `1` or `2`
     if ('show' === $task) {
+        if ($has_comment_guard_extension) {
+            if (isset($data['email']) && \is_file($f = \LOT . \DS . 'x' . \DS . 'comment.guard' . \DS . 'email.txt')) {
+                if (\filesize($f) > 0) {
+                    $lines = [];
+                    $test = $data['email'];
+                    foreach (\stream($f) as $v) {
+                        if ($test === ($v = \trim($v))) {
+                            continue;
+                        }
+                        $lines[] = $v;
+                    }
+                    \file_put_contents($f, \implode("\n", $lines), \LOCK_EX);
+                }
+            }
+            if (isset($data['ip']) && \is_file($f = \LOT . \DS . 'x' . \DS . 'comment.guard' . \DS . 'ip.txt')) {
+                if (\filesize($f) > 0) {
+                    $lines = [];
+                    $test = $data['ip'];
+                    foreach (\stream($f) as $v) {
+                        if ($test === ($v = \trim($v))) {
+                            continue;
+                        }
+                        $lines[] = $v;
+                    }
+                    \file_put_contents($f, \implode("\n", $lines), \LOCK_EX);
+                }
+            }
+        }
         $data['status'] = \Is::user() === $author ? 1 : 2;
         if (\is_writable($file) && \is_int(\file_put_contents($file, \To::page($data)))) {
             \Alert::success('Comment marked as not spam.');
